1da3f4daa786973f3fc9bb469d3356ffc77863e3
Abhishek Sharma <hello.abhishek@outlook.com>
2019-10-28 19:45:56 +0530

    worksss

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+var flag = 0;
 var loadImage = function (input) {
     var preview = document.getElementById('friends');
     if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        preview.setAttribute('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            preview.setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
     } else {
-      preview.setAttribute('src', './assets/placeholder.png');
+        preview.setAttribute('src', './assets/placeholder.png');
     }
     let desc = document.getElementsByClassName('desc')[0];
     desc.style.opacity = 0;
+    flag = 1;
 };
+
+var showDesc = function () {
+    if (flag === 1) {
+        let desc = document.getElementsByClassName('desc')[0];
+        desc.style.opacity = 1;
+    }
+}
+
+var hideDesc = function () {
+    if (flag === 1) {
+        let desc = document.getElementsByClassName('desc')[0];
+        desc.style.opacity = 0;
+    }
+}
 
 function downloadImage() {
     let to = "Letter for " + document.getElementById('person').innerHTML + ".png";
@@ -29,3 +45,6 @@ function downloadImage() {
         document.getElementsByClassName('primary')[0].href = imageUrl;
     });
 }
+
+
+document.getElementById('friends')
