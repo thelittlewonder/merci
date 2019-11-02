645d34946838f7271af1e593dd6a2fef22894e7d
Abhishek Sharma <hello.abhishek@outlook.com>
2019-11-02 13:58:08 +0530

    remove update

--- main.js
+++ main.js
@@ -52,9 +52,10 @@ var downloadImage = function() {
     });
 }
 
+/*
 let a = document.getElementsByClassName('editable');
 for(i=0;i<a.length;i++){
     a[i].addEventListener("input", function () {
         downloadImage();
     }, false)
-}
+}*/
